5d89a2a13b428ad6b01f0f70cd426594da7b78f7
Leonardo Rossi <leonardo.rossi@gmail.com>
2022-11-10 14:52:50 +0100

    0.1.18

--- src/stories/DropDown.stories.jsx
+++ src/stories/DropDown.stories.jsx
@@ -12,8 +12,8 @@ export const Default = Template.bind({})
 Default.args = {
   header: 'My Menu',
   items: [
-    <span>Menu 1</span>,
-    <span>Menu 2</span>,
-    <span>This is a very long menu item</span>
+    <span key='1'>Menu 1</span>,
+    <span key='2'>Menu 2</span>,
+    <span key='3'>This is a very long menu item</span>
   ]
 }
