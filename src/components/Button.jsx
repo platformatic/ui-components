0ab2d7bb4d21fb9b4adf013054cac0926f5a08c5
Leonardo Rossi <leonardo.rossi@gmail.com>
2022-12-01 15:13:48 +0100

    Adds disabled status for button
    Signed-off-by: Leonardo Rossi <leonardo.rossi@gmail.com>

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 import styles from './Button.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 export default function Button (props) {
-  const { icon, label, primary, color } = props
+  const { icon, label, primary, color, disabled } = props
   let buttonClass = 'primary'
   if (!primary) {
     buttonClass = 'secondary'
@@ -15,7 +15,7 @@ export default function Button (props) {
     colorClass = 'red'
   }
   return (
-    <button className={`${styles.button} ${styles[buttonClass + '-' + colorClass]}`} {...props}>
+    <button className={`${styles.button} ${styles[buttonClass + '-' + colorClass]} ${disabled ? styles.disabled : null}`} disabled={disabled} {...props}>
       {icon ? <FontAwesomeIcon icon={icon} className='mr-2' data-testid='button-icon' /> : null}
       <span>{label}</span>
     </button>
--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -1,5 +1,6 @@
 'use strict'
 import { faCheck } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react'
 import Button from '../components/Button'
 
 export default {
@@ -68,4 +69,39 @@ SecondaryRed.args = {
   primary: false,
   label: 'Secondary Red',
   color: 'red'
+}
+
+const DisabledTemplate = (args) => {
+  const [enabled, setEnabled] = useState(false)
+  return (
+    <div className='flex flex-col gap-y-6 text-white'>
+      <div>
+        <Button {...args} disabled={!enabled} onClick={() => alert('clicked')} />
+        <span className='ml-4 text-xl'>👈 This button is {enabled ? 'enabled' : 'disabled'}</span>
+      </div>
+      <div>
+        <Button label='Toggle Disabled' primary='true' onClick={() => setEnabled(!enabled)} />
+      </div>
+
+    </div>
+
+  )
+}
+
+export const DisabledGreen = DisabledTemplate.bind({})
+
+DisabledGreen.args = {
+  primary: true,
+  label: 'A simple button',
+  color: 'green',
+  disabeld: true
+}
+
+export const DisabledRed = DisabledTemplate.bind({})
+
+DisabledRed.args = {
+  primary: true,
+  label: 'A simple button',
+  color: 'red',
+  disabeld: true
 }
