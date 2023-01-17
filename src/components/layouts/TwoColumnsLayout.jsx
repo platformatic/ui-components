7aed0f64c8885b8438ccd68d4afe6fa20f9c6669
Antonio Sonis <anthonysonis@gmail.com>
2023-01-17 17:08:30 +0100

    fix: using const variable
    Signed-off-by: Antonio Sonis <anthonysonis@gmail.com>

--- src/components/layouts/TwoColumnsLayout.jsx
+++ src/components/layouts/TwoColumnsLayout.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 import styles from './TwoColumnsLayout.module.css'
 
 export default function TwoColumnsLayout ({ children, gridTemplate = 'columns' }) {
+  const className = styles[`${gridTemplate}`]
   return (
-    <div className={styles[`${gridTemplate}`]}>
+    <div className={className}>
       {children}
     </div>
   )
