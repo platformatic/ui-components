fd1f63925dbf938326a32ea6c2c21e26745162dc
Antonio Sonis <anthonysonis@gmail.com>
2023-01-20 11:02:09 +0100

    chore: update layout of Buttons
    Signed-off-by: Antonio Sonis <anthonysonis@gmail.com>

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,19 +4,17 @@ import React from 'react'
 import styles from './Button.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 export default function Button (props) {
-  const { icon, label, primary, color, disabled } = props
-  let buttonClass = 'primary'
-  if (!primary) {
-    buttonClass = 'secondary'
-  }
+  // buttonClass: primary|secondary|transparent
+  // colorClass: green|white|red
+  // size: small|medium|large|extra-large
+  const { icon, label, buttonClass = 'secondary', color = 'green', disabled, size = 'large', bold = false, ...rest } = props
+  let className = `${styles.button} ${styles[buttonClass + '-' + color]} ${styles['button-' + size]}`
+  if (disabled) className += ` ${styles.disabled}`
+  if (bold) className += ` ${styles.fontBold}`
 
-  let colorClass = 'green'
-  if (color === 'red') {
-    colorClass = 'red'
-  }
   return (
-    <button className={`${styles.button} ${styles[buttonClass + '-' + colorClass]} ${disabled ? styles.disabled : null}`} disabled={disabled} {...props}>
-      {icon ? <FontAwesomeIcon icon={icon} className='mr-2' data-testid='button-icon' /> : null}
+    <button className={className} disabled={disabled} alt={label} {...rest}>
+      {icon ? <FontAwesomeIcon icon={icon} className={`${styles['margin-right-' + size]}`} data-testid='button-icon' /> : null}
       <span>{label}</span>
     </button>
   )
