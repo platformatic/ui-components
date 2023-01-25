eb1573a1d1f1517267faa794cd93c76ea5c50309
Antonio Sonis <anthonysonis@gmail.com>
2023-01-25 10:36:52 +0100

    fix: adding picture on dropdown menu
    Signed-off-by: Antonio Sonis <anthonysonis@gmail.com>

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -5,7 +5,7 @@ import styles from './DropDown.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronLeft, faChevronDown } from '@fortawesome/free-solid-svg-icons'
 export default function DropDown (props) {
-  const { header, items, align = 'left' } = props
+  const { pictureUrl, header, items, align = 'left' } = props
   const [open, setOpen] = useState(false)
   function handleOpen () {
     setOpen(!open)
@@ -13,6 +13,7 @@ export default function DropDown (props) {
   return (
     <div className={`${styles.dropDown} ${styles[align]}`}>
       <span className={styles.header} onClick={handleOpen}>
+        {pictureUrl && <img src={pictureUrl} height={32} width={32} className={styles.picture} />}
         {header}
         {!open && <FontAwesomeIcon className={styles.arrow} icon={faChevronLeft} />}
         {open && <FontAwesomeIcon className={styles.arrow} icon={faChevronDown} />}
