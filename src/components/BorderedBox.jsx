830f0f6f4d557b13331488235a874bdc6767740c
Antonio Sonis <anthonysonis@gmail.com>
2023-02-09 22:44:10 +0100

    fix: adding background color to BorderedBox as prop
    Signed-off-by: Antonio Sonis <anthonysonis@gmail.com>

--- src/components/BorderedBox.jsx
+++ src/components/BorderedBox.jsx
@@ -5,11 +5,12 @@ import styles from './BorderedBox.module.css'
 import commonStyles from './Common.module.css'
 import React from 'react'
 export default function BorderedBox (props) {
-  const { classes, color, children } = props
+  const { classes, color, children, backgroundColor = 'dark-blue' } = props
   const borderColor = getColor('border', color)
+  const className = `${styles.borderedBox} ${commonStyles.bordered} ${classes} ${borderColor} ` + styles[`background-color-${backgroundColor}`]
 
   return (
-    <div className={`${styles.borderedBox} ${commonStyles.bordered} ${classes} ${borderColor}`}>
+    <div className={className}>
       {children}
     </div>
   )
