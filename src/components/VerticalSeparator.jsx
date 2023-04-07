27b470efc20f55c5593164632eca1d1e62124011
Antonio Sonis <anthonysonis@gmail.com>
2023-04-07 11:37:45 +0200

    chore: adding mx to VerticalSeparator
    Signed-off-by: Antonio Sonis <anthonysonis@gmail.com>

--- src/components/VerticalSeparator.jsx
+++ src/components/VerticalSeparator.jsx
@@ -1,5 +1,5 @@
 'use strict'
 import React from 'react'
 export default function VerticalSeparator () {
-  return <span className='text-dark-green my-auto mx-0'> | </span>
+  return <span className='text-dark-green my-auto mx-5'> | </span>
 }
