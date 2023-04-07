02107da6edebbc457d2966fd9e9e3d4ea73bca64
Antonio Sonis <anthonysonis@gmail.com>
2023-04-07 10:34:55 +0200

    adding some rework to VerticalSeparator
    Signed-off-by: Antonio Sonis <anthonysonis@gmail.com>

--- src/components/VerticalSeparator.jsx
+++ src/components/VerticalSeparator.jsx
@@ -1,5 +1,5 @@
 'use strict'
 import React from 'react'
 export default function VerticalSeparator () {
-  return <span className='text-dark-green m-3'> | </span>
+  return <span className='text-dark-green my-auto mx-0'> | </span>
 }
