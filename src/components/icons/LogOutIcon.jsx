c66a0730f60482390b884d6aa7b86620da7da397
Antonio Sonis <anthonysonis@gmail.com>
2023-04-20 18:15:32 +0200

    chore: data-tip to platformatic icon
    Signed-off-by: Antonio Sonis <anthonysonis@gmail.com>

--- src/components/icons/LogOutIcon.jsx
+++ src/components/icons/LogOutIcon.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import styles from './Icons.module.css'
 import { COLORS_ICON, SIZES } from '../constants'
 
-const LogOutIcon = ({ color, size }) => {
+const LogOutIcon = ({ color, size, tip }) => {
   const className = `${styles.noShrinkForFlex} ` + styles[`${color}`]
   let icon = <></>
 
@@ -16,6 +16,7 @@ const LogOutIcon = ({ color, size }) => {
           viewBox='0 0 16 16'
           fill='none'
           xmlns='http://www.w3.org/2000/svg'
+          data-tip={tip}
           className={className}
         >
           <path d='M8.38687 5.5V3C8.38687 2.44772 7.93915 2 7.38687 2H3C2.44772 2 2 2.44772 2 3V13C2 13.5523 2.44771 14 3 14H7.38687C7.93915 14 8.38687 13.5523 8.38687 13V11.5' stroke='none' strokeLinecap='round' />
@@ -32,6 +33,7 @@ const LogOutIcon = ({ color, size }) => {
           viewBox='0 0 24 24'
           fill='none'
           xmlns='http://www.w3.org/2000/svg'
+          data-tip={tip}
           className={className}
         >
           <path d='M12.5803 8.25V4C12.5803 3.44772 12.1326 3 11.5803 3H4C3.44772 3 3 3.44772 3 4V20C3 20.5523 3.44772 21 4 21H11.5803C12.1326 21 12.5803 20.5523 12.5803 20V17.25' stroke='none' strokeWidth={1.5} strokeLinecap='round' />
@@ -48,6 +50,7 @@ const LogOutIcon = ({ color, size }) => {
           viewBox='0 0 40 40'
           fill='none'
           xmlns='http://www.w3.org/2000/svg'
+          data-tip={tip}
           className={className}
         >
           <path d='M20.9672 13.75V6C20.9672 5.44772 20.5195 5 19.9672 5H6C5.44772 5 5 5.44772 5 6V34C5 34.5523 5.44772 35 6 35H19.9672C20.5195 35 20.9672 34.5523 20.9672 34V28.75' stroke='none' strokeWidth={2} strokeLinecap='round' />
@@ -71,12 +74,17 @@ LogOutIcon.propTypes = {
   /**
    * Size
    */
-  size: PropTypes.oneOf(SIZES)
+  size: PropTypes.oneOf(SIZES),
+  /**
+   * tip
+   */
+  tip: PropTypes.string
 }
 
 LogOutIcon.defaultProps = {
   color: 'main-dark-blue',
-  size: 'medium'
+  size: 'medium',
+  tip: ''
 }
 
 export default LogOutIcon
