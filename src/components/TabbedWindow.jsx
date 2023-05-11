f50003cc4358153bd268dcaa81af18a3343b43ea
Antonio Sonis <anthonysonis@gmail.com>
2023-05-11 13:19:33 +0200

    chore: adding serviceIcon and Tabbed window handled differently
    Signed-off-by: Antonio Sonis <anthonysonis@gmail.com>

--- src/components/TabbedWindow.jsx
+++ src/components/TabbedWindow.jsx
@@ -1,25 +1,28 @@
 'use strict'
 import React, { useEffect, useState } from 'react'
+import PropTypes from 'prop-types'
 import styles from './TabbedWindow.module.css'
 
-export default function TabbedWindow (props) {
-  const { tabs, defaultSelected = 0, callbackSelected = () => {} } = props
-  const [selected, setSelected] = useState(defaultSelected)
+function TabbedWindow ({ tabs, keySelected, callbackSelected }) {
   const headers = []
+  const keys = []
   const components = []
   tabs.forEach((tab) => {
     headers.push(tab.label)
+    keys.push(tab.key)
     components.push(tab.component)
   })
-  let currentComponent = components[selected]
+  const [selected, setSelected] = useState(keySelected || tabs?.at(0).key || '')
+  const [currentComponent, setCurrentComponent] = useState(components[0])
 
   useEffect(() => {
-    currentComponent = components[selected]
+    const indexKey = keys.findIndex(key => key === selected)
+    setCurrentComponent(components[indexKey])
   }, [selected])
 
   function setCurrentTab (index) {
-    setSelected(index)
-    callbackSelected(index)
+    setSelected(keys[index])
+    callbackSelected(keys[index])
   }
 
   return (
@@ -27,13 +30,37 @@ export default function TabbedWindow (props) {
       <div className={styles['tabs-header']}>
         {headers.map((header, index) => {
           return (
-            <span onClick={() => setCurrentTab(index)} key={index} className={`${styles.tab} ${selected === index ? styles['selected-tab'] : ''}`}>
+            <span onClick={() => setCurrentTab(index)} key={index} className={`${styles.tab} ${selected === keys[index] ? styles['selected-tab'] : ''}`}>
               {header}
             </span>
           )
         })}
       </div>
-      <div className={styles['tabs-content']}>{currentComponent()}</div>
+      <div className={styles['tabs-content']}>{currentComponent}</div>
     </div>
   )
 }
+
+TabbedWindow.propTypes = {
+  /**
+   * tabs
+   */
+  tabs: PropTypes.array,
+  /**
+   * keySelected
+   */
+  keySelected: PropTypes.string,
+  /**
+   * callbackSelected
+   */
+  callbackSelected: PropTypes.func
+
+}
+
+TabbedWindow.defaultProps = {
+  tabs: [],
+  keySelected: '',
+  callbackSelected: () => {}
+}
+
+export default TabbedWindow
--- src/stories/TabbedWindow.stories.jsx
+++ src/stories/TabbedWindow.stories.jsx
@@ -14,15 +14,37 @@ const Template = (args) => {
   )
 }
 export const Default = Template.bind({})
+
 Default.args = {
   tabs: [{
     label: 'First',
+    key: 'first',
     component: () => <TextWithLabel label='First component' text='Lorem ipsum dolor sit amet, consectetur adipiscing elit.' />
   }, {
     label: 'Second',
+    key: 'second',
     component: () => <TextWithLabel label='Second component' text='Sed et dui facilisis, molestie urna sed, volutpat nibh' />
   }, {
     label: 'Third',
+    key: 'third',
     component: () => <TextWithLabel label='Third component' text='ivamus est nisl, maximus aliquet urna eu, consequat semper nisi' />
   }]
+}
+
+export const DefaultSelected = Template.bind({})
+DefaultSelected.args = {
+  tabs: [{
+    label: 'First',
+    key: 'first',
+    component: () => <TextWithLabel label='First component' text='Lorem ipsum dolor sit amet, consectetur adipiscing elit.' />
+  }, {
+    label: 'Second',
+    key: 'second',
+    component: () => <TextWithLabel label='Second component' text='Sed et dui facilisis, molestie urna sed, volutpat nibh' />
+  }, {
+    label: 'Third',
+    key: 'third',
+    component: () => <TextWithLabel label='Third component' text='ivamus est nisl, maximus aliquet urna eu, consequat semper nisi' />
+  }],
+  keySelected: 'second'
 }
