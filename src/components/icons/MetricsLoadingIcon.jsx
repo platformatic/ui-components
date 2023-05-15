7c3f5a366629e7ed08e0b3434d6c6c07aba23958
Antonio Sonis <anthonysonis@gmail.com>
2023-05-15 16:44:19 +0200

    fix: typo string instead of numbers
    Signed-off-by: Antonio Sonis <anthonysonis@gmail.com>

--- src/components/icons/MetricsLoadingIcon.jsx
+++ src/components/icons/MetricsLoadingIcon.jsx
@@ -13,8 +13,8 @@ const MetricsLoadingIcon = ({ color, size }) => {
     case EXTRA_LARGE:
       icon = (
         <svg
-          width='104'
-          height='104'
+          width={104}
+          height={104}
           viewBox='0 0 104 104'
           fill='none'
           xmlns='http://www.w3.org/2000/svg'
