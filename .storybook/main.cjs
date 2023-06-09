0ecbc5d6545b31710d61068986bf9e89c5aa3207
Aasim Attia <u5r0@pm.me>
2023-06-09 21:35:24 +0300

    Storybook v7 upgrade
    Signed-off-by: Aasim Attia <u5r0@pm.me>

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,39 +1,36 @@
 module.exports = {
   "stories": [
-    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.mdx", 
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-controls"
+    "@storybook/addon-links", 
+    "@storybook/addon-essentials", 
+    "@storybook/addon-interactions", 
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  "framework": {
+    name: "@storybook/react-vite",
+    options: {}
   },
   "features": {
     "storyStoreV7": true
   },
-  "webpackFinal": async (config) => {
+  "webpackFinal": async config => {
     config.module.rules.push({
       test: /\.css$/,
-      use: [
-        {
-          loader: 'postcss-loader',
-          options: {
-            postcssOptions: {
-              plugins: [
-                require('tailwindcss'),
-                require('autoprefixer'),
-              ],
-            },
-          },
-        },
-      ],
-      include: path.resolve(__dirname, '../'),
-    })
-    return config
+      use: [{
+        loader: 'postcss-loader',
+        options: {
+          postcssOptions: {
+            plugins: [require('tailwindcss'), require('autoprefixer')]
+          }
+        }
+      }],
+      include: path.resolve(__dirname, '../')
+    });
+    return config;
   },
-}
+  docs: {
+    autodocs: true
+  }
+};
