d5e46c90ced0507267d2df10b9a48cd245d10d65
Antonio Sonis <anthonysonis@gmail.com>
2023-06-30 16:43:19 +0200

    feat: including textArea
    Signed-off-by: Antonio Sonis <anthonysonis@gmail.com>

--- src/components/forms/index.js
+++ src/components/forms/index.js
@@ -3,8 +3,9 @@ import Input from './Input'
 import InputWithSeparator from './InputWithSeparator'
 import Preview from './Preview'
 import Select from './Select'
+import TextArea from './TextArea'
 import ToggleSwitch from './ToggleSwitch'
 
 export default {
-  Field, Input, InputWithSeparator, Preview, Select, ToggleSwitch
+  Field, Input, InputWithSeparator, Preview, Select, TextArea, ToggleSwitch
 }
---
+++ src/stories/forms/TextArea.stories.jsx
@@ -0,0 +1,73 @@
+'use strict'
+import React, { useState } from 'react'
+import TextArea from '../../components/forms/TextArea'
+
+const divStyle = {
+  width: '100%',
+  height: 'auto',
+  padding: '20px',
+  backgroundColor: 'white'
+}
+
+export default {
+  title: 'Platformatic/Forms/TextArea',
+  component: TextArea,
+  decorators: [
+    (Story) => (
+      <div style={divStyle}>
+        <Story />
+      </div>
+    )
+  ]
+}
+
+const Template = (args) => <TextArea {...args} />
+
+export const Default = Template.bind({})
+
+Default.args = {
+  name: 'test',
+  placeholder: 'Platformatic'
+}
+
+export const DefaultInvalid = Template.bind({})
+
+DefaultInvalid.args = {
+  name: 'test',
+  placeholder: 'Platformatic',
+  borderColor: 'main-dark-blue',
+  errorMessage: 'This is an error message'
+}
+
+const TemplateValuedAndAlertChange = (args) => {
+  const [value, setValue] = useState('')
+
+  function handleChange (event) {
+    setValue(event.target.value)
+  }
+
+  return (
+    <>
+      <p>Value of the input {value}</p>
+      <TextArea {...args} value={value} onChange={handleChange} />
+    </>
+  )
+}
+
+export const ValuedAndAlertChange = TemplateValuedAndAlertChange.bind({})
+
+ValuedAndAlertChange.args = {
+  name: 'test',
+  placeholder: 'Platformatic'
+}
+
+export const IconAfter = Template.bind({})
+
+IconAfter.args = {
+  name: 'test',
+  placeholder: 'Platformatic',
+  afterIcon: {
+    iconName: 'CircleAddIcon',
+    color: 'error-red'
+  }
+}
