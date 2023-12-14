6a5a4e2f163a5e72b601743133b9682ec54b59c0
Antonio Sonis <anthonysonis@gmail.com>
2023-12-14 18:46:23 +0100

    fix: updating search bar and adding story
    Signed-off-by: Antonio Sonis <anthonysonis@gmail.com>

---
+++ src/stories/SearchBarV2.stories.jsx
@@ -0,0 +1,24 @@
+'use strict'
+import SearchBarV2 from '../components/SearchBarV2'
+export default {
+  title: 'Platformatic/SearchBarV2',
+  component: SearchBarV2
+}
+const Template = (args) => {
+  return (
+    <>
+      <SearchBarV2 {...args} />
+    </>
+
+  )
+}
+export const Standard = Template.bind({})
+
+Standard.args = {
+  onChange: (value) => {
+    console.log('Current search: ' + value)
+  },
+  onSubmit: (value) => {
+    alert('Query value: ' + value)
+  }
+}
