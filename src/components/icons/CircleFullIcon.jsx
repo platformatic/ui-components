6181bdc749276eb56ef10208021aabb4eb89ccc0
Antonio Sonis <anthonysonis@gmail.com>
2024-10-23 10:57:46 +0200

    fix: problem with size of CirleFullIcon Mini
    Signed-off-by: Antonio Sonis <anthonysonis@gmail.com>

--- src/components/icons/CircleFullIcon.jsx
+++ src/components/icons/CircleFullIcon.jsx
@@ -38,14 +38,14 @@ const CircleFullIcon = ({
     case TINY:
       icon = (
         <svg
-          width={4}
-          height={4}
-          viewBox='0 0 4 4'
+          width={8}
+          height={8}
+          viewBox='0 0 8 8'
           fill='none'
           xmlns='http://www.w3.org/2000/svg'
           className={className}
         >
-          <circle cx={4} cy={4} r={3} fill='none' className={filledClassName} />
+          <circle cx={4} cy={4} r={4} fill='none' className={filledClassName} />
         </svg>
       )
       break
