d759db0708a39b75ffbbff555486e9258ad20dfa
Antonio Sonis <anthonysonis@gmail.com>
2024-10-31 14:07:58 +0100

    fix: updating AppIcon
    Signed-off-by: Antonio Sonis <anthonysonis@gmail.com>

--- src/components/icons/AppIcon.jsx
+++ src/components/icons/AppIcon.jsx
@@ -29,7 +29,8 @@ const AppIcon = ({
           xmlns='http://www.w3.org/2000/svg'
           className={className}
         >
-          <path d='M8 2L13.1962 5V11L8 14L2.80385 11V5L8 2Z' stroke='none' strokeLinejoin='round' />
+          <path d='M2.5 5V10.8722L8 14M8 14L13.5 10.8722V5M8 14V8.00001' stroke='none' strokeLinecap='round' strokeLinejoin='round' />
+          <path d='M8 7.71423L2.5 4.85711L8 2L13.5 4.85711L8 7.71423Z' stroke='none' strokeLinecap='round' strokeLinejoin='round' />
         </svg>
       )
       break
@@ -43,8 +44,8 @@ const AppIcon = ({
           xmlns='http://www.w3.org/2000/svg'
           className={className}
         >
-          <path d='M12 3L19.7942 7.5V16.5L12 21L4.20577 16.5V7.5L12 3Z' stroke='none' strokeWidth={1.5} strokeLinejoin='round' />
-
+          <path d='M3.75 7.5V16.3083L12 21M12 21L20.25 16.3083V7.5M12 21V12' stroke='none' strokeWidth={1.5} strokeLinecap='round' strokeLinejoin='round' />
+          <path d='M12 11.5713L3.75 7.28567L12 3L20.25 7.28567L12 11.5713Z' stroke='none' strokeWidth={1.5} strokeLinecap='round' strokeLinejoin='round' />
         </svg>
       )
       break
@@ -58,7 +59,9 @@ const AppIcon = ({
           xmlns='http://www.w3.org/2000/svg'
           className={className}
         >
-          <path d='M20 5L32.9904 12.5V27.5L20 35L7.00962 27.5V12.5L20 5Z' stroke='none' strokeWidth={2} strokeLinejoin='round' />
+          <path d='M6.25 12.4999V27.1805L20 35M20 35L33.75 27.1805V12.4999M20 35V20' stroke='none' strokeWidth={2} strokeLinecap='round' strokeLinejoin='round' />
+          <path d='M20 19.2856L6.25 12.1428L20 5L33.75 12.1428L20 19.2856Z' stroke='none' strokeWidth={2} strokeLinecap='round' strokeLinejoin='round' />
+
         </svg>
       )
       break
